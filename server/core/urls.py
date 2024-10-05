69f417a7cf7fafb4e62ffa8ce0078f596f608b34
Kada-quantum <kada02ak1@outlook.com>
2024-10-05 15:55:37 +0200

    remove server

--- server/core/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.index, name='index'),
-    path('home/', views.home, name="home"),
-    path('flowchart/', views.flowchart, name="flowchart"),
-]
